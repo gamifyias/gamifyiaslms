c4b478a135792ba70dcc0b123f7eb130382df43e
Tirth <tirth.11072008@gmail.com>
2025-12-08 19:19:40 +0530

    Final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
-const _inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
+const robotoMono = Roboto_Mono({ subsets: ["latin"], variable: "--font-mono" })
 
 export const metadata: Metadata = {
   title: "Gamify IAS",
@@ -17,17 +18,17 @@ export const metadata: Metadata = {
       },
     ],
   },
-    generator: 'Tirth Joshi'
+  generator: "Tirth Joshi",
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`font-sans antialiased`}>
+    <html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
+      <body className="font-sans antialiased">
         {children}
         <Analytics />
       </body>
